c414f9acd686da8ec382bfae087dec851dbf4d71
AdityaP183 <adityaprasad1828@gmail.com>
2025-10-25 13:14:23 +0530

    feat(auth, user): implement JWT authentication and user registration logic

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,39 @@
-import { Injectable } from "@nestjs/common";
+import { ConflictException, Injectable, Logger } from "@nestjs/common";
 import { RegisterDto } from "./dto/register.dto";
 import { UserService } from "src/user/user.service";
+import bcrypt from "bcrypt";
+import { JwtService } from "@nestjs/jwt";
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly userSerivce: UserService) {
-        this.userSerivce = userSerivce;
-    }
+    private readonly logger = new Logger(AuthService.name);
+    constructor(
+        private readonly userSerivce: UserService,
+        private readonly jwtService: JwtService,
+    ) {}
 
     async register(registerDto: RegisterDto) {
         const user = await this.userSerivce.getUserByEmail(registerDto.email);
-        return { message: "User registered successfully", user };
+        if (user) {
+            throw new ConflictException("User with that email already exists");
+        }
+
+        const saltRounds = 10;
+        const hashedPassword = await bcrypt.hash(
+            registerDto.password,
+            saltRounds,
+        );
+
+        const newUser = await this.userSerivce.createUser({
+            ...registerDto,
+            password: hashedPassword,
+        });
+
+        this.logger.log(`User created: ${newUser.email}`);
+
+        const payload = { sub: newUser.id, username: newUser.email };
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        };
     }
 }
